c60f9236f7bdf92b681402c63784c4fb8e155a6f
meraeugene <villalon.andrew123@gmail.com>
2025-07-03 17:36:40 +0800

    update params page

--- app/(user)/watch/[id]/page.tsx
+++ app/(user)/watch/[id]/page.tsx
@@ -5,14 +5,16 @@ import WatchClient from "./WatchClient";
 export default async function WatchPage({
   params,
 }: {
-  params: { id: string; slug: string };
+  params: Promise<{ id: string }>;
 }) {
   const user = await getCurrentUser();
   if (!user) redirect("/");
 
+  const recommendationId = (await params).id;
+
   return (
     <main className="min-h-screen bg-black text-white pt-28 pb-16 px-15">
-      <WatchClient currentUser={user} recommendationId={params.id} />
+      <WatchClient currentUser={user} recommendationId={recommendationId} />
     </main>
   );
 }
