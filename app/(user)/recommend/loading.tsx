6d7e4b45e20cf7d343b0ac5a8c13d48cbb16a09d
meraeugene <villalon.andrew123@gmail.com>
2025-07-09 11:50:25 +0800

    adjust loading border

--- app/(user)/recommend/loading.tsx
+++ app/(user)/recommend/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-black">
-      <div className="w-8 h-8 border-4 border-white/30 border-t-white rounded-full animate-spin" />
+      <div className="w-8 h-8 border-3 border-white/30 border-t-white rounded-full animate-spin" />
     </div>
   );
 }
