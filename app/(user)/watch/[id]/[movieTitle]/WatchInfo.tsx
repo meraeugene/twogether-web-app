ebd231aefc4dc19895c81d7d31d66a858ca9fa33
meraeugene <villalon.andrew123@gmail.com>
2025-07-21 08:47:50 +0800

    fix: adjust button styling in WatchInfo component for improved layout

--- app/(user)/watch/[id]/[movieTitle]/WatchInfo.tsx
+++ app/(user)/watch/[id]/[movieTitle]/WatchInfo.tsx
@@ -94,7 +94,7 @@ export default function WatchInfo({
             {isAiRecommendation && !alreadyRecommended && (
               <button
                 onClick={() => setOpen(true)}
-                className="cursor-pointer w-fit inline-flex items-center gap-2 bg-white text-black hover:bg-white/90 transition text-sm md:text-base px-4 py-2 rounded-md font-medium"
+                className="cursor-pointer w-fit inline-flex items-center gap-2 bg-white text-black hover:bg-white/90 transition text-sm  px-4 py-2 rounded-md "
               >
                 <Sparkles className="w-4 h-4" />
                 Recommend This!
