b4c7c0db5ee92d305c93b227ce436baf3270f1ac
meraeugene <villalon.andrew123@gmail.com>
2025-08-11 15:04:57 +0800

    fix: adjust gap in Navbar component for improved layout consistency

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -91,7 +91,7 @@ export function Navbar({ user }: { user: CurrentUser | null }) {
         ${isExpanded ? "rounded-3xl" : "rounded-full"}`}
     >
       {/* Desktop Menu */}
-      <div className="relative z-10 flex justify-between gap-12 text-nowrap items-center px-4 md:px-8 2xl:px-6 py-3">
+      <div className="relative z-10 flex justify-between gap-10 text-nowrap items-center px-4 md:px-8 2xl:px-6 py-3">
         <Link
           href="/"
           onClick={() => setMenuOpen(false)}
