a4ee07c65f59db4fa13070a72b78f7fe728d4d9e
Andrew Villalon <villalon.andrew123@gmail.com>
2025-08-27 15:20:38 +0800

    Update TMDBSuggestions.tsx

--- app/(root)/tmdb/watch/[id]/[slug]/TMDBSuggestions.tsx
+++ app/(root)/tmdb/watch/[id]/[slug]/TMDBSuggestions.tsx
@@ -32,8 +32,6 @@ export default function TMDBSuggestions({
     fetcher
   );
 
-  console.log(data);
-
   const setCurrentTMDB = useTMDBWatch((s) => s.setCurrentTMDB);
 
   const suggestions = data?.results || [];
@@ -58,7 +56,7 @@ export default function TMDBSuggestions({
           <button
             onClick={() => setPage((p) => Math.min(totalPages, p + 1))}
             disabled={page >= totalPages}
-            className="p-3 rounded-full cursor-pointer bg-gray-800 text-white hover:bg-gray-700 disabled:opacity-40 disabled:hover:bg-gray-800 transition-colors"
+            className="p-2 md:p-3 rounded-full cursor-pointer bg-gray-800 text-white hover:bg-gray-700 disabled:opacity-40 disabled:hover:bg-gray-800 transition-colors"
           >
             <ChevronRight className="w-5 h-5" />
           </button>
