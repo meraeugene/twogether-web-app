4e3e47152f79fbc214dfdf00dc20f669958c130e
meraeugene <villalon.andrew123@gmail.com>
2025-09-01 10:05:23 +0800

    fix: update movies API endpoint to use discover for improved data fetching

--- app/api/tmdb/trending/route.ts
+++ app/api/tmdb/trending/route.ts
@@ -8,7 +8,7 @@ export async function GET() {
   try {
     // --- Movies ---
     const moviesRes = await fetch(
-      `${BASE_URL}/movie/popular?language=en-US&region=US&page=1&api_key=${API_KEY}`,
+      `${BASE_URL}/discover/movie?language=en-US&region=US&include_adult=false&certification_country=US&certification.lte=PG-13&sort_by=popularity.desc&api_key=${API_KEY}`,
       { cache: "force-cache", next: { revalidate: 86400 } }
     );
     const moviesData = await moviesRes.json();
