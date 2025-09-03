ca22a0a6a03bb3fc39cbee45f0c0d42ca5d67271
meraeugene <villalon.andrew123@gmail.com>
2025-09-03 11:30:20 +0800

    feat: enhance loading component with skeleton UI for improved user experience

--- app/(root)/binge/loading.tsx
+++ app/(root)/binge/loading.tsx
@@ -1,7 +1,20 @@
 const loading = () => {
   return (
-    <div className="text-white text-7xl min-h-screen flex items-center justify-center">
-      loading...
+    <div className="min-h-screen bg-black flex flex-col  lg:px-24 xl:px-32 2xl:px-26 xl:pt-34 px-7 pt-28  pb-16 text-white">
+      <div className="h-4 w-[30%] bg-white/10 rounded mb-6 animate-pulse" />
+      {Array.from({ length: 3 }).map((_, i) => (
+        <div key={`skeleton-section-${i}`}>
+          <div className="h-8 w-72 bg-white/10 rounded mb-6 animate-pulse" />
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 xl:grid-cols-5 gap-8">
+            {Array.from({ length: 6 }).map((_, j) => (
+              <div
+                key={`skeleton-card-${i}-${j}`}
+                className="aspect-[2/3] w-full rounded-md bg-white/10 animate-pulse"
+              />
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
